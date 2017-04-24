c387634eeb41e1e49d91813018ab73c336d1fbcb
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-04-24 10:04:02 +0200

    TeamBuilting: Changing the messages routing key

--- src/main/java/org/manuel/teambuilting/messages/PlayerRegisteredEvent.java
+++ src/main/java/org/manuel/teambuilting/messages/PlayerRegisteredEvent.java
@@ -2,6 +2,7 @@ package org.manuel.teambuilting.messages;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
@@ -14,6 +15,7 @@ import java.util.Date;
  */
 @Data
 @AllArgsConstructor
+@Builder
 public class PlayerRegisteredEvent {
 
     public static final String ROUTING_KEY = "player.cud.registered";
