3f2e21a70b1e985c2e8824490f4a80ed261ceae5
Manuel Doncel Martos <manuel.doncel.martos@powerhouse.nl>
2017-04-26 21:04:44 +0200

    Removing to associate player to user when player created

--- src/main/java/org/manuel/teambuilting/sports/config/RabbitMQConfig.java
+++ src/main/java/org/manuel/teambuilting/sports/config/RabbitMQConfig.java
@@ -2,12 +2,11 @@ package org.manuel.teambuilting.sports.config;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.text.SimpleDateFormat;
 
 /**
  * @author Manuel Doncel Martos
@@ -18,14 +17,19 @@ public class RabbitMQConfig  {
 
     private static final String TIMESTAMP_FORMAT = "yyyy-MM-dd\'T\'HH:mm:ss.SSSZ";
 
+    @Bean
+	public ObjectMapper objectMapper() {
+		final ObjectMapper jsonObjectMapper = new ObjectMapper();
+		jsonObjectMapper.configure(DeserializationFeature.ACCEPT_FLOAT_AS_INT, false);
+		jsonObjectMapper.findAndRegisterModules();
+		return jsonObjectMapper;
+	}
+
     @Bean(name = "eventMessageConverter")
     public MessageConverter messageConverter() {
         final Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
         // Jackson deserialization point issue
-        final ObjectMapper jsonObjectMapper = new ObjectMapper();
-        jsonObjectMapper.configure(DeserializationFeature.ACCEPT_FLOAT_AS_INT, false);
-        jsonObjectMapper.setDateFormat(new SimpleDateFormat(TIMESTAMP_FORMAT));
-        converter.setJsonObjectMapper(jsonObjectMapper);
+        converter.setJsonObjectMapper(objectMapper());
         return converter;
     }
 
