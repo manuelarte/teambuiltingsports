bb6bc75a14eb242815a38d8f1691e237a6402637
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-05-01 22:10:37 +0200

    TeamBuiltingSports: Not allowing more entries for one sport

--- src/main/java/org/manuel/teambuilting/sports/services/command/impl/PlayerToTeamSportDetailsCommandServiceImpl.java
+++ src/main/java/org/manuel/teambuilting/sports/services/command/impl/PlayerToTeamSportDetailsCommandServiceImpl.java
@@ -1,12 +1,16 @@
 package org.manuel.teambuilting.sports.services.command.impl;
 
 import org.manuel.teambuilting.core.services.command.AbstractCommandService;
+import org.manuel.teambuilting.exceptions.ErrorCode;
+import org.manuel.teambuilting.exceptions.ValidationRuntimeException;
 import org.manuel.teambuilting.sports.model.PlayerToTeamSportDetails;
 import org.manuel.teambuilting.sports.repositories.PlayerToTeamSportDetailsRepository;
 import org.manuel.teambuilting.sports.services.command.PlayerToTeamSportDetailsCommandService;
+import org.manuel.teambuilting.sports.services.query.PlayerToTeamSportDetailsQueryService;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
+import java.util.Optional;
 
 /**
  * @author Manuel Doncel Martos
@@ -16,9 +20,21 @@ import javax.inject.Inject;
 class PlayerToTeamSportDetailsCommandServiceImpl extends AbstractCommandService<PlayerToTeamSportDetails, String, PlayerToTeamSportDetailsRepository> implements
 	PlayerToTeamSportDetailsCommandService {
 
+    private final PlayerToTeamSportDetailsQueryService playerToTeamSportDetailsQueryService;
+
 	@Inject
-	public PlayerToTeamSportDetailsCommandServiceImpl(final PlayerToTeamSportDetailsRepository playerToTeamSportDetailsRepository) {
+	public PlayerToTeamSportDetailsCommandServiceImpl(final PlayerToTeamSportDetailsRepository playerToTeamSportDetailsRepository,
+                                                      final PlayerToTeamSportDetailsQueryService playerToTeamSportDetailsQueryService) {
 		super(playerToTeamSportDetailsRepository);
+		this.playerToTeamSportDetailsQueryService = playerToTeamSportDetailsQueryService;
 	}
+
+	@Override
+	protected void beforeSave(final PlayerToTeamSportDetails playerToTeamSportDetails) {
+        final Optional<PlayerToTeamSportDetails> previousEntry = playerToTeamSportDetailsQueryService.findPlayerDetailsForSport(playerToTeamSportDetails.getPlayerId(), playerToTeamSportDetails.getSport());
+        if (previousEntry.isPresent() && !previousEntry.get().getId().equals(playerToTeamSportDetails.getId())) {
+            throw new ValidationRuntimeException(ErrorCode.SPORT_DETAILS_FOR_PLAYER_ALREADY_REGISTERED, playerToTeamSportDetails.getPlayerId(), playerToTeamSportDetails.getSport());
+        }
+    }
 
 }
