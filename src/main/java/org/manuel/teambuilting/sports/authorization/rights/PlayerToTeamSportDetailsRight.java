0caf9a12ae2fd272394b8dfdc049577f62e6c3ff
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-08-03 22:38:33 +0200

    Checking rights

---
+++ src/main/java/org/manuel/teambuilting/sports/authorization/rights/PlayerToTeamSportDetailsRight.java
@@ -0,0 +1,15 @@
+package org.manuel.teambuilting.sports.authorization.rights;
+
+import org.manuel.teambuilting.rights.EntityDependentAppRight;
+import org.manuel.teambuilting.sports.model.PlayerToTeamSportDetails;
+
+/**
+ * @author Manuel Doncel Martos
+ * @since 03/08/2017.
+ */
+public class PlayerToTeamSportDetailsRight {
+
+    public static class Create implements EntityDependentAppRight<PlayerToTeamSportDetails> {
+
+    }
+}
---
+++ src/main/java/org/manuel/teambuilting/sports/authorization/rights/Rights.java
@@ -0,0 +1,12 @@
+package org.manuel.teambuilting.sports.authorization.rights;
+
+import org.manuel.teambuilting.rights.AppRight;
+
+/**
+ * @author Manuel Doncel Martos
+ * @since 03/08/2017.
+ */
+public enum Rights implements AppRight {
+
+    SEND_EMAIL, WHATEVER_THAT_DOES_NOT_INVOLVE_ENTITIES;
+}
