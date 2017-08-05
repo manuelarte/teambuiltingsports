1b0615b0db4e623ce23d68c1b9863f18c3fb8d4b
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-08-05 21:41:38 +0200

    Checking rights

--- src/main/java/org/manuel/teambuilting/sports/TeamBuiltingSportsApplication.java
+++ src/main/java/org/manuel/teambuilting/sports/TeamBuiltingSportsApplication.java
@@ -1,16 +1,13 @@
 package org.manuel.teambuilting.sports;
 
-import org.manuel.teambuilting.core.config.CoreConfig;
 import org.manuel.teambuilting.core.config.EnableCoreFunctionalities;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableCoreFunctionalities
-@Import({CoreConfig.class})
 public class TeamBuiltingSportsApplication {
 
 	public static void main(final String[] args) {
